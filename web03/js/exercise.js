da8d0d161907ddc2909b0e30e102de635d49d2fe
서다솜 <somying@naver.com>
2021-03-15 17:14:04 +0900

    연습

--- web03/js/exercise.js
+++ web03/js/exercise.js
@@ -13,3 +13,37 @@ function exercise1(node) {
         }
     }
 }
+
+function exercise8(node) {
+    let date = new Date();
+    if (node.children.length <= 1) {
+        for(let year = date.getFullYear(); year > date.getFullYear() - 20; year--) {
+            let option = $("<option></option>")
+            option.attr("value", year);
+            option.text(year);
+            $(node).append(option);
+        }
+    }
+
+    // node.addEventListener('change', funtion() {
+    //     if(this.lastChild.value == this.value) {
+    //         for(let year = this.value - 1; year > this.value - 11; year--) {
+    //             let option = document.createElement('option');
+    //             option.value = year;
+    //             option.innerText = year;
+    //             this.appendChild(option);
+    //         }
+    //     }
+    // })
+
+    $(node).on('change', function() {
+        if ($(this).children().last().val() == $(this).val()) {
+            for(let year = $(this).val() - 1; year > $(this).val() - 11; year--) {
+                let option = $("<option></option>")
+                option.attr("value", year);
+                option.text(year);
+                $(this).append(option);
+            }
+        }
+    });
+}
